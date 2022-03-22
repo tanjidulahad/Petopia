e3a9fc74f51b536e8d42ef91fc867f259a6ff2e9
mauryanavi <navneet@sigaramtech.com>
2022-03-22 18:17:41 +0530

    Build fix

--- pages/account/savedplaces.js
+++ pages/account/savedplaces.js
@@ -1,26 +1,31 @@
-<>
-    <p className="text-xl text-gray-900 font-bold">
-        {' '}
-        Delivery Address
-    </p>
-    <div className="grid lg:grid-cols-2 md:grid-cols-1  gap-6 my-5">
-        <div className="w-full rounded-lg shadow">
-            <Address type={`Home`} />
-        </div>
 
-        <div className="w-full rounded-lg shadow">
-            <Address type={`Work`} />
-        </div>
-        <div className="w-full rounded-lg shadow">
-            <Address type={`Work`} />
-        </div>
-    </div>
-    <div className="flex cursor-pointer ">
-        <BsPlusCircle className="text-red-500" size={30} />
+export default function () {
+    return null
+    // return (
+    //     <>
+    //         <p className="text-xl text-gray-900 font-bold">
+    //             {' '}
+    //             Delivery Address
+    //         </p>
+    //         <div className="grid lg:grid-cols-2 md:grid-cols-1  gap-6 my-5">
+    //             <div className="w-full rounded-lg shadow">
+    //                 <Address type={`Home`} />
+    //             </div>
 
-        <p className="text-lg text-red-500  font-semibold ml-4">
-            {' '}
-            Add New Address{' '}
-        </p>
-    </div>
-</>
+    //             <div className="w-full rounded-lg shadow">
+    //                 <Address type={`Work`} />
+    //             </div>
+    //             <div className="w-full rounded-lg shadow">
+    //                 <Address type={`Work`} />
+    //             </div>
+    //         </div>
+    //         <div className="flex cursor-pointer ">
+    //             <BsPlusCircle className="text-red-500" size={30} />
+
+    //             <p className="text-lg text-red-500  font-semibold ml-4">
+    //                 {' '}
+    //                 Add New Address{' '}
+    //             </p>
+    //         </div>
+    //     </>)
+}
--- pages/account/wallet.js
+++ pages/account/wallet.js
@@ -1,9 +1,13 @@
-<div className="grid grid-cols-1   gap-6 ">
-    <div className="w-full rounded-lg shadow">
-        <Wallet />
-    </div>
+export default function () {
+    return null
+    // retrun(<div className="grid grid-cols-1   gap-6 ">
+    //     <div className="w-full rounded-lg shadow">
+    //         {/* <Wallet /> */}
+    //     </div>
 
-    <div className="w-full rounded-lg shadow">
-        <Transaction />
-    </div>
-</div> 
+    //     <div className="w-full rounded-lg shadow">
+    //         {/* <Transaction /> */}
+    //     </div>
+    // </div>)
+
+} 

8a44d5b53b937cbf03aac28728b7c769524fb033
mauryanavi <navneet@sigaramtech.com>
2022-03-24 18:47:42 +0530

    After adding dynamic storeId, first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
+import { useEffect, useState } from 'react';
 import Layout from '@components/layout';
 import store from '@redux/store';
 import { Provider } from 'react-redux';
-
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router'
 import NProgress from 'nprogress'; //nprogress module
 import 'nprogress/nprogress.css'
 
@@ -30,6 +30,18 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [redirected, setRedirected] = useState(false);
+
+  useEffect(() => {
+    const path = router.asPath;
+    // console.log((/#!(\/.*)$/.exec(router.asPath) || []), 'fadfasdfsadfds');
+    if (path != '/' && !path.includes('[')) {
+      router.replace(path);
+      setRedirected(true)
+    }
+  }, [router.isReady])
+
   return (
     <Provider store={store}>
       <Layout>

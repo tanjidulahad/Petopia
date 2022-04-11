16824e3307bef75237991757527aca21c9f8aff5
mohammedazhar.1598 <mohammedazhar.1598@gmail.com>
2022-04-11 10:40:09 +0530

    added root page

--- pages/[name]/index.js
+++ pages/[name]/index.js
@@ -6,13 +6,39 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { useState } from 'react';
+import fetcher from "../../redux/utility";
 
 export default function Name() {
 
     const [showModal, setShowModal] = useState(false)
+    const [input, setInput] = useState()
+    const [stores, setStores] = useState([])
+    const router = useRouter()
 
-    const handleChange = () => {
+    const handleChange = (e) => {
+        setInput({ ...input, [e.target.name]: [e.target.value] })
 
+        const filter = stores.filter(item =>
+            item.store_name.toLowerCase().includes(input?.input)
+        )
+
+
+        if (input.input[0].length <= 1) {
+            getStores()
+        }
+        else {
+            setStores(filter)
+        }
+    }
+
+    useEffect(() => {
+        getStores()
+    }, [])
+
+    const getStores = async () => {
+        const response = await fetcher('GET', '?r=stores/get-sponsored-stores')
+        console.log('responsse', response)
+        setStores(response.data)
     }
 
 
@@ -156,49 +182,19 @@ export default function Name() {
                 <div className="flex flex-col items-center p-5 lg:block md:block lg:px-24 md:px-24">
                     <p className="text-black font-bold text-xl mb-3 lg:ml-3 md:ml-3 lg:mb-3 md:mb-3">Featured Shops</p>
                     <div className="flex flex-row flex-wrap justify-between lg:justify-start lg:ml-3">
+                        {stores.map((store, index) => {
+                            return (
+                                <div className="flex flex-col pt-2 w-1/2 lg:w-1/4 md:w-1/4 items-center lg:items-start md:items-start max-h-60 min-h-36 " key={index}>
+                                    <div className='flex items-start '>
+                                        <img src={store?.logo_img_url} className="min-h-28 max-h-28" />
+                                    </div>
+                                    <p className="text-lg font-semibold mt-4">{store.store_name}</p>
+                                    <p className="text-gray-300 text-sm w-44" style={{ fontSize: '12px' }}>{store.store_desc}.<span className='lg:hidden md:hidden'> Visit now</span></p>
+                                    <button className='hidden lg:block md:block bg-[#4A0037CC] text-white border border-[#4A0037CC] hover:bg-white hover:text-[#4A0037CC] cursor-pointer p-1 rounded-lg pl-2 pr-2 ml-12 text-sm mt-2'>Visit Store</button>
+                                </div>
 
-
-                        <div className="flex flex-col pt-2 w-1/2 lg:w-1/4 md:w-1/4 items-center lg:items-start md:items-start max-h-60 min-h-36 ">
-                            <div className='flex items-start '>
-                                <img src="https://img.freepik.com/free-psd/luxury-beauty-logo-mockup_4513-551.jpg?w=2000" className="min-h-28 max-h-28" />
-                            </div>
-                            <p className="text-lg font-semibold mt-4">Robinhood</p>
-                            <p className="text-gray-300 text-sm w-44" style={{ fontSize: '12px' }}>Gadgets and accessories at best price.<span className='lg:hidden md:hidden'> Visit now</span></p>
-                            <button className='hidden lg:block md:block bg-[#4A0037CC] text-white border border-[#4A0037CC] hover:bg-white hover:text-[#4A0037CC] cursor-pointer p-1 rounded-lg pl-2 pr-2 ml-12 text-sm mt-2'>Visit Store</button>
-                        </div>
-
-
-
-                        <div className="flex flex-col pt-2 w-1/2 lg:w-1/4 md:w-1/4 items-center lg:items-start md:items-start max-h-60 min-h-36 ">
-                            <div className='flex items-start '>
-                                <img src="https://img.freepik.com/free-psd/luxury-beauty-logo-mockup_4513-551.jpg?w=2000" className="min-h-28 max-h-28" />
-                            </div>
-                            <p className="text-lg font-semibold mt-4">Robinhood</p>
-                            <p className="text-gray-300 text-sm w-44" style={{ fontSize: '12px' }}>Gadgets and accessories at best price.<span className='lg:hidden md:hidden'> Visit now</span></p>
-                            <button className='hidden lg:block md:block bg-[#4A0037CC] text-white border border-[#4A0037CC] hover:bg-white hover:text-[#4A0037CC] cursor-pointer p-1 rounded-lg pl-2 pr-2 ml-12 text-sm mt-2'>Visit Store</button>
-                        </div>
-
-
-                        <div className="flex flex-col pt-2 w-1/2 lg:w-1/4 md:w-1/4 items-center lg:items-start md:items-start max-h-60 min-h-36 ">
-                            <div className='flex items-start '>
-                                <img src="https://img.freepik.com/free-psd/luxury-beauty-logo-mockup_4513-551.jpg?w=2000" className="min-h-28 max-h-28" />
-                            </div>
-                            <p className="text-lg font-semibold mt-4">Robinhood</p>
-                            <p className="text-gray-300 text-sm w-44" style={{ fontSize: '12px' }}>Gadgets and accessories at best price.<span className='lg:hidden md:hidden'> Visit now</span></p>
-                            <button className='hidden lg:block md:block bg-[#4A0037CC] text-white border border-[#4A0037CC] hover:bg-white hover:text-[#4A0037CC] cursor-pointer p-1 rounded-lg pl-2 pr-2 ml-12 text-sm mt-2'>Visit Store</button>
-                        </div>
-
-
-                        <div className="flex flex-col pt-2 w-1/2 lg:w-1/4 md:w-1/4 items-center lg:items-start md:items-start max-h-60 min-h-36 ">
-                            <div className='flex items-start '>
-                                <img src="https://img.freepik.com/free-psd/luxury-beauty-logo-mockup_4513-551.jpg?w=2000" className="min-h-28 max-h-28" />
-                            </div>
-                            <p className="text-lg font-semibold mt-4">Robinhood</p>
-                            <p className="text-gray-300 text-sm w-44" style={{ fontSize: '12px' }}>Gadgets and accessories at best price.<span className='lg:hidden md:hidden'> Visit now</span></p>
-                            <button className='hidden lg:block md:block bg-[#4A0037CC] text-white border border-[#4A0037CC] hover:bg-white hover:text-[#4A0037CC] cursor-pointer p-1 rounded-lg pl-2 pr-2 ml-12 text-sm mt-2'>Visit Store</button>
-                        </div>
-
-
+                            )
+                        })}
                     </div>
                 </div>
                 <div className="p-5 lg:flex lg:flex-col lg:px-24 md:flex md:flex-col md:px-24">

4a4c8a9c4b19152250f5dfcd32694a249a2f989d
mauryanavi <navneet@sigaramtech.com>
2022-07-15 16:26:28 +0530

    👍

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -72,7 +72,9 @@ const Navbar = ({ user, cart, displaySettings, openAuth, logOut, getShopInfo, ge
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill inline" viewBox="0 0 16 16">
                           <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
                         </svg>
-                        <span className='text-sm lg:text-base font-semibold tracking-tight ml-2 '>No. 9, 11th Cross St, Indira Nagar, Adyar, Chennai, Tamil Nadu 600020.</span>
+                        <span className='text-sm lg:text-base font-semibold tracking-tight ml-2 '>
+                          {info.address}{info.city && ', ' + info.city}{info.state && ', ' + info.state}{info.country && ', ' + info.country}
+                        </span>
                       </>
                     }
                   </div>
